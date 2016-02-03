6228cabf17ee531256d80457c4310671f6a62cf5
Maxim Fedorenko (Macbook) <maxim.fedorenko@texturacorp.com>
2016-02-03 03:05:53 +0300

    Add GenerateRandomGene() and CalculateFitness() functions.

--- src/genetic_diophant/genetic.h
+++ src/genetic_diophant/genetic.h
@@ -1,4 +1,8 @@
+#include <cstdlib>
+
 #include <array>
+#include <random>
+#include <stdexcept>
 #include <vector>
 
 namespace genetic {
@@ -34,16 +38,71 @@ class Diophant {
   public:
     Diophant( AlleleArray coefficients, Allele result ) :
         coefficients_( coefficients ),
-        result_( result ) {
+        result_( result ),
+        population_( kPopulationCount ) {
+
     }
 
-    Gene Solve() {
-        return Gene();
+    Gene Solve( unsigned int iteration_count_max = 50 ) {
+        //std::random_device rd;
+        //mt_generator_.seed( rd() );
+
+        mt_generator_.seed( 42 );  /// \< \fixme for debug only
+
+        for ( auto i = 0; i < kPopulationCount; ++i ) {
+            population_.push_back( GenerateRandomGene() );
+        }
+
+        auto iteration_count = 0;
+
+        while ( iteration_count++ < iteration_count_max ) {
+            CalculateFitness();
+            auto it = std::find_if( population_.cbegin(), population_.cend(), []( const Gene & gene ) {
+                return gene.fitness == 0;
+            } );
+
+            if ( it != population_.cend() ) {
+                return *it;
+            }
+
+
+        }
+
+        throw new std::runtime_error( "Could not find the result gene" );
     }
 
   private:
     AlleleArray coefficients_;
     Allele result_;
+
+    GeneContainer population_;
+
+    mutable std::mt19937 mt_generator_;
+
+    Gene GenerateRandomGene() const {
+        std::uniform_int_distribution<Allele> dis( 1, result_ );
+        auto gene = Gene();
+
+        for ( auto &allele : gene.alleles ) {
+            allele = dis( mt_generator_ );
+        }
+
+        return gene;
+    }
+
+    void CalculateFitness() {
+        for ( auto &gene : population_ ) {
+            auto res = Allele( 0 );
+
+            for ( auto i = 0; i < kAllelesCount; ++i ) {
+                res += gene.alleles[i] * coefficients_[i];
+            }
+
+            auto fitness = std::abs( static_cast<int>( result_ - res ) );
+
+            gene.fitness = fitness;
+        }
+    }
 };
 
 }  // namespace genetic
